c666fcffccb1ab438e0d49a3162d633fce5339ae
Felix Stephen Anthuvan <felix_s@dell.com>
2021-03-02 14:12:51 +0545

    ansible module

---
+++ ansible_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python        # This is called 'shebang' or 'hashbang'
+
+# Copyright: (c) 2021, Your Name <YourName@example.org>						# it depends on your organization
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)	# it depends on your organization
+
+
+from __future__ import (absolute_import, division, print_function)	# python2 / python3
+__metaclass__ = type							# declaration of metaclass
+
+
+
+
