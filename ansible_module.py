8f0c526a8c6c283a165d857859eb3c406616a09a
Felix Stephen Anthuvan <felix_s@dell.com>
2021-03-02 15:15:39 +0545

    ansible module

--- ansible_module.py
+++ ansible_module.py
@@ -4,9 +4,71 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)	# it depends on your organization
 
 
-from __future__ import (absolute_import, division, print_function)	# python2 / python3
-__metaclass__ = type							# declaration of metaclass
+from __future__ import (absolute_import, division, print_function)	# python2 / python3 compatability issue
+__metaclass__ = type							# module level declaration of metaclass
 
 
 
+DOCUMENTATION = """
+---
+module: your_new_module_name
+
+short_description: short description of your module.
+
+description:
+  - long description of your module.
+  - in case multiple sentences.
+
+version_added: "3.0.0"
+
+options:
+  option_name:
+    description: option desription details
+    required: True
+    type: str
+"""
+
+EXAMPLES = """
+---
+
+- name: "collecting the inventory"
+  namespace.collection.your_new_module_name:
+    host: 127.0.0.1
+    username: localhost
+    password: *********
+"""
+
+RETURN = """
+---
+msg:
+  type: str
+  returned: always
+  description: the output message always returned
+  sample: Successfully fetched the information
+error_info:
+  type: dict
+  returned: on error
+  description: returned in case of error
+  sample: error details
+"""
+
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec={
+            "name": {"required": True, "type": "str"},
+        },
+        supports_check_mode=False,
+    )
+
+   # processing
+
+   module.exit_json(msg="successfully fetched the details")
+
+
+if __name__ == "__main__"
+    main()
 
